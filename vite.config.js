850cd62e77b53a78f2162aba3ec05a0bc6f5c231
RtyXmd <rongtianyang@inin88.com>
2022-08-03 10:45:42 +0800

    add inspect

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import virtual from "./plugins/virtual-module";
+import inspect from "vite-plugin-inspect";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), virtual()],
+  plugins: [vue(), virtual(), inspect()],
 });
